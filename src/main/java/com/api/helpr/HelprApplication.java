bc8d9bd7114a11d07117e7c1edb1e444906a4b26
victoricoma <victorsena3010@gmail.com>
2022-05-11 10:33:35 -0300

    Projeto inicial com Spring com Dependências instaladas

---
+++ src/main/java/com/api/helpr/HelprApplication.java
@@ -0,0 +1,13 @@
+package com.api.helpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/helpr/HelprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.helpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
