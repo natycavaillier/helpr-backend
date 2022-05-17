9f4a853145b314ceb055b53b2e9509a145ee3182
victoricoma <victorsena3010@gmail.com>
2022-05-17 17:08:20 -0300

    Orientação dos filtros com Between, In, Like e contextos de filtros para update.

---
+++ MySQL/empregados/AulaFiltrosAvancadosEmpregados.sql
@@ -0,0 +1,40 @@
+USE empregados_db;
+
+/*Concatenador de regras*/
+SELECT * FROM empregado WHERE est_civil='C' OR est_civil='U' OR est_civil='D'; 
+
+/* Filtros por meio de faixas*/
+SELECT * FROM empregado WHERE cod_depto BETWEEN 3 and 5;
+
+/* Filtros por proximidade de string */
+SELECT * FROM empregado WHERE nome LIKE "An%";
+SELECT * FROM empregado WHERE nome LIKE "%o";
+SELECT * FROM empregado WHERE nome LIKE "%Julia%";
+
+/* Amostra no espaço de um campo */
+SELECT * FROM empregado WHERE dt_nascimento IN("1994-10-19","1990-01-25");
+SELECT * FROM empregado WHERE dt_nascimento IN("1994-10-19","1990-01-25") AND dt_admissao LIKE "2009%";
+SELECT * FROM empregado WHERE dt_nascimento > "1990-01-01";
+SELECT * FROM empregado WHERE cod_empregado IN(SELECT cod_empregado FROM empregado WHERE dt_nascimento LIKE "198%");
+
+/* Alterando Registros do Banco */
+UPDATE empregado SET nome = "Maria Eduarda Di Senna" WHERE cod_empregado = 13;
+UPDATE empregado SET nome = "Marcela Marcondes" WHERE cod_empregado = 14;
+select * from empregado;
+
+/* Dados Voláteis e os Dados Físicos */
+SELECT nome, FLOOR(DATEDIFF(NOW(), dt_nascimento) / 365) AS idade FROM empregado;
+
+/* Exercícos de Filtros e Cálculos*/
+/* 1.Alterar a idade da Rosemay para  1992-02-15. */
+/* 2.Calcular o tempo de empresa de todos os funcionários. */
+/* 3.Calcular a idade de todos os dependentes. */
+/* 5.Filtar os funcionarios que ganham mais de 1 salários e que nasceram na decada de 90 */
+
+
+
+
+
+
+
+
