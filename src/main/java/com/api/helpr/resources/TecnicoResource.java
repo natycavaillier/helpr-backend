c70a676d0642370c21d0eb6309ba6f64f94fdfe2
victoricoma <victorsena3010@gmail.com>
2022-05-30 17:50:25 -0300

    Modelagem das entregas para front com uso de DTO.

--- src/main/java/com/api/helpr/resources/TecnicoResource.java
+++ src/main/java/com/api/helpr/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.helpr.domain.Tecnico;
+import com.api.helpr.domain.dtos.TecnicoDTO;
 import com.api.helpr.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
---
+++ src/main/java/com/api/helpr/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.api.helpr.resources.exceptions;
+
+public class ResourceExceptionHandler {
+
+}
