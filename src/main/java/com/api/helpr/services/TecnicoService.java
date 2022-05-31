ef6a8808da457e6db7d4c0fe0340b94f87198de1
victoricoma <victorsena3010@gmail.com>
2022-05-31 17:46:07 -0300

    Listagem de todos os técnicos pelo método do JPA finddAll

--- src/main/java/com/api/helpr/services/TecnicoService.java
+++ src/main/java/com/api/helpr/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.api.helpr.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado: " + id));
+	}
+
+	public List<Tecnico> findAllTecnicos() {
+		return repository.findAll();
 	}
 }
