ac3ae3b1071808843d26979034c7eada5a399fca
victoricoma <victorsena3010@gmail.com>
2022-06-06 09:45:03 -0300

    Criação do update do banco via PUT e com as validações.

--- src/main/java/com/api/helpr/domain/Pessoa.java
+++ src/main/java/com/api/helpr/domain/Pessoa.java
@@ -15,6 +15,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.api.helpr.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -28,6 +30,7 @@ public abstract class Pessoa implements Serializable{
 	protected Integer id;
 	protected String nome;
 	
+	@CPF
 	@Column(unique = true)
 	protected String cpf;
 	
