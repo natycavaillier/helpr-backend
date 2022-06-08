48cc0f3507a9a842be9e91120fc04c5d25cb21c4
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-06-08 14:19:01 -0300

    Adição da busca de cliente por id

---
+++ src/main/java/com/api/helpr/services/ClienteService.java
@@ -0,0 +1,31 @@
+package com.api.helpr.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.helpr.domain.Cliente;
+import com.api.helpr.repositories.ClienteRepository;
+import com.api.helpr.repositories.PessoaRepository;
+import com.api.helpr.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	//Injeção de dependencias
+	//Vínculo com repositório ClienteRepository
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	//Método de busca de um cliente por um ID no banco
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado: " + id));
+	}
+	
+}
