f8e18fd906896553020ea20bd819e3a7b89b10a0
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-06-19 19:00:25 -0300

    Fix login

--- src/main/java/com/api/helpr/services/UserDetailsServiceImpl.java
+++ src/main/java/com/api/helpr/services/UserDetailsServiceImpl.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.api.helpr.domain.Pessoa;
 import com.api.helpr.repositories.PessoaRepository;
 import com.api.helpr.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
 	@Autowired
